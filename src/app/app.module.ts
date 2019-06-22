9ec22607b17ffbfbe735e1c9960e762a14f560eb
TDR <denis.tdr@gmail.com>
2019-06-22 18:36:43 +0300

    fix issue from last conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
 import {LoadingBarHttpClientModule} from '@ngx-loading-bar/http-client';
 import {CoreModule} from './core/core.module';
 import {SharedModule} from './shared/shared.module';
@@ -16,7 +16,7 @@ import {SharedModule} from './shared/shared.module';
     imports: [
         BrowserModule,
         AppRoutingModule,
-        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+        ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
         CoreModule,
         SharedModule.forRoot(),
         LoadingBarHttpClientModule,
