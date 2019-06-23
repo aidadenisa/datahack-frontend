3653b36bf950621984d52149ae422ff96f6a7d99
TDR <denis.tdr@gmail.com>
2019-06-23 14:05:25 +0300

    wip rooms table (landing page)

--- src/app/features/booking/booking.module.ts
+++ src/app/features/booking/booking.module.ts
@@ -4,23 +4,32 @@ import { CommonModule } from '@angular/common';
 import { BookingRoutingModule } from './booking-routing.module';
 import { BookingLandingPageComponent } from './pages/landing-page/booking-landing-page.component';
 import { RoomsTableComponent } from './components/rooms-table/rooms-table.component';
-import { MatTableModule } from '@angular/material';
+import { MatButtonModule, MatDialogModule, MatListModule, MatTableModule } from '@angular/material';
 import { SharedModule } from '../../shared/shared.module';
+import { RoomDetailsDialogComponent } from './components/room-details-dialog/room-details-dialog.component';
 
 const matImports = [
-    MatTableModule];
+    MatTableModule,
+    MatDialogModule,
+];
 
 @NgModule({
     declarations: [
         BookingLandingPageComponent,
         RoomsTableComponent,
+        RoomDetailsDialogComponent,
     ],
     imports: [
         CommonModule,
         BookingRoutingModule,
         ...matImports,
         SharedModule,
+        MatButtonModule,
+        MatListModule,
     ],
+    entryComponents: [
+        RoomDetailsDialogComponent,
+    ]
 })
 export class BookingModule {
 }
--- src/app/shared/models/location-model.ts
+++ src/app/shared/models/location-model.ts
@@ -1,7 +1,9 @@
 import { Entity } from '../../core/models/entity.model';
 import { RoomModel } from './room-model';
+import { OrganizationModel } from './organization-model';
 
 export interface LocationModel extends Entity {
     name: string;
     rooms?: RoomModel[];
+    organization?: OrganizationModel;
 }
